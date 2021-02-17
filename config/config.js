fce9c1eeb58a958ddf64193a3df1a68228afdf45
Nick Stull <73801944+NickStull@users.noreply.github.com>
2021-02-17 09:49:38 -0600

    jaws fixes

--- config/config.js
+++ config/config.js
@@ -17,10 +17,7 @@ module.exports =
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    use_env_variable: "JAWSDB_URL",
+    dialect: "mysql"
   }
 }
